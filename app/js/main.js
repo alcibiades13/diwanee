48415016c82fba3fd553a0e4e961df6b292ec689
alcibiades13 <branislav.teofilovic@gmail.com>
2016-02-18 17:04:59 +0100

    adding hamburger menu and smooth scrolling

--- app/js/main.js
+++ app/js/main.js
@@ -3,37 +3,32 @@ $(document).ready(function () {
     $('.hamburger').on('click', function(e) {
         e.preventDefault();
 
-        $('.main-nav').toggleClass('visible');
+        $('.main-nav').slideToggle(150);
     });
 
-        $('.thumbn-img').on('click', function () {
-            var nextImgSrc = $(this).prop('src');
-            $('.img-big').attr('src', nextImgSrc);
+    if($( window ).width() < 320) {
+
+        $('.main-nav li a').on('click', function (e) {
+            e.preventDefault();
+            $('.main-nav').slideUp(400);
         });
 
-        $('.thumbn-img').click(function() {
-          $('#shutter').addClass('on');
-          $('audio')[0].play();
-          setTimeout(function() {
-            $('#shutter').removeClass('on');
-          }, 35*2+45);/* Shutter speed (double & add 45) */
-        });
+    }
 
-        $('.slide-next').on('click', slideToNext);
 
-        function slideToNext() {
-            var currImgSrc = $('.img-big').attr('src');
+    // smooth scrolling
 
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 750);
+            return false;
+          }
         }
-
-
-
-
-        $('.thumbn-img').hover(function() {
-            var a = Math.random() * 10 - 5;
-            $(this).css('transform', 'rotate(' + a + 'deg) ');
-        }, function() {
-            $(this).css('transform', 'none');
-        });
+      });
 
 });
